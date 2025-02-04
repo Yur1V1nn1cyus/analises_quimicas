1db288f09c315c2c0a7eba2eb4ac3efd6bf0cef6
Yur1V1nn1cyus <158053329+Yur1V1nn1cyus@users.noreply.github.com>
2025-02-04 10:33:59 -0300

    Resolvido a parte de gerar um grafico

--- lei_lambert/app.py
+++ lei_lambert/app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 #primeiro definimos variaveis para os valores a serem necessarios
 #a= absorbancia
 #b= coef_abs_molar
@@ -8,7 +9,7 @@ import pandas as pd
 #usuario / maquina ira inserir os dados aqui
 
 ##
-def calculo_conc (carregar_df='C:/Users/marci/Documents/Program/python_chemistry/lei_lambert/dados_populados.csv', descarregar_df='C:/Users/marci/Documents/Program/python_chemistry/lei_lambert/retorno.csv' ):
+def calculo_conc (carregar_df, descarregar_df):
     valoresDF = pd.read_csv(carregar_df)
     a = float(valoresDF.loc[0, 'Absorbância'])
     b = float(valoresDF.loc[0, 'Coeficiente de Absorção Molar'])
@@ -24,5 +25,33 @@ def calculo_conc (carregar_df='C:/Users/marci/Documents/Program/python_chemistry
             print(f"Arquivo salvo com sucesso: {descarregar_df}")
         except ZeroDivisionError:
             print("Erro: Divisão por zero nao e aceitavel")
+        except Exception as e:
+            print(f"Erro: {e}")
+        except ValueError as e:
+            print(f"Erro: {e}")
+        except FileNotFoundError as e:
+            print(f"Erro: {e}")
+        
+carregar_df = input("Insira o caminho do arquivo JSON: ")
+descarregar_df = input("Insira o caminho do arquivo que queira salvar o arquivo csv: ")
+calculo_conc(carregar_df, descarregar_df)
 
-calculo_conc()
+ver_plot = input("Deseja visualizar o arquivo csv gerado? (s/n): ")
+if ver_plot == "s":
+    try:
+        df = pd.read_csv(carregar_df)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(df['Absorbância'], df['Coeficiente de Absorção Molar'], color ='r', marker='o')
+        ax.set_xlabel('Absorbância')
+        ax.set_ylabel('Coeficiente de Absorção Molar')
+        ax.set_zlabel('Concentração')
+        ax.set_title('Gráfico de Concentração em Dispercao 3D')
+        save_plot = input("Deseja salvar o gráfico? (s/n): ")
+        if save_plot == "s":
+            fig.savefig('Gráfico de Concentração em Dispercao 3D.png')
+        plt.show()
+    except FileNotFoundError as e:
+        print(f"Erro: {e}")
+    except Exception as e:
+        print(f"Erro: {e}")
